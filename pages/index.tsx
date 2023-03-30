310da7cfc424c7e0643e7f62438203d50a293e6b
limonmiridha <limonmiridha100@gmail.com>
2023-03-30 21:34:03 +0600

    finalize design

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,24 +7,64 @@ import { Github, Twitter } from "@/components/shared/icons";
 import WebVitals from "@/components/home/web-vitals";
 import ComponentGrid from "@/components/home/component-grid";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video: any = videoRef.current;
+
+    const rotateAfterSeconds = 1.86;
+    const rotateAngle = -55;
+
+    const handleTimeUpdate = () => {
+      if (video.currentTime >= rotateAfterSeconds) {
+        video.style.transform = `rotateZ(${rotateAngle}deg) `;
+        // video.style.transform = `rotateX(45deg) `;
+        video.removeEventListener("timeupdate", handleTimeUpdate);
+      }
+    };
+
+    video.addEventListener("timeupdate", handleTimeUpdate);
+
+    return () => {
+      video.removeEventListener("timeupdate", handleTimeUpdate);
+    };
+  }, []);
   return (
     <Layout>
       {/* Hero section */}
       <div className="bg-scroll ">
         <div className="m-auto max-w-screen-lg ">
-          <div className=" z-10 mt-[72px] text-center decoration-[#f5f5f7] ">
+          <div className="  text-center decoration-[#f5f5f7] ">
             <h1 className=" mb-2 font-semibold md:text-[24px]">
               iPhone 12 Pro
             </h1>
             <h2 className=" text-[56px] font-semibold md:text-[80px]">
               Its a leap year.
             </h2>
-            <video autoPlay muted width={1000}>
-              <source src="/hero.mp4" type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
+            <div
+              style={{
+                transformStyle: "preserve-3d",
+              }}
+              className="overflow-hidden"
+            >
+              <video
+                autoPlay
+                muted
+                width={800}
+                ref={videoRef}
+                // style={{ transform: "rotateZ(-65deg)" }}
+                style={{
+                  transition: "transform 0.5s ease",
+                  // transform: "rotateX(45deg)",
+                }}
+                className="mx-auto -mt-32"
+                src="/hero.mp4"
+                // type="video/mp4"
+              />
+            </div>
 
             <h3 className="mx-auto w-4/6 text-2xl font-bold text-gray-400">
               5G goes Pro. A14 Bionic rockets past every other smartphone chip.
