55418765f8b7177a160ce0106a1ce35444b57a74
unknown <shajibhasan7325@gmail.com>
2023-04-03 14:46:53 +0600

    responsive

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -3,23 +3,37 @@ import { motion } from "framer-motion";
 import { ParallaxBanner } from "react-scroll-parallax";
 import { BannerLayer } from "react-scroll-parallax/dist/components/ParallaxBanner/types";
 import { OpacityEffectAfter } from "src/utils/RSParallax";
+import WavyText from "../Animations/wave";
+import React from "react";
 
 type Props = {
   boxVariant: any;
   videoRef: any;
 };
 
+// make a function which will return true when given seconds have passed
+
 function Hero({ boxVariant, videoRef }: Props) {
   return (
     <div className="text-center decoration-[#f5f5f7]">
-      <motion.div variants={boxVariant} initial="hidden" animate="visible">
+      {/* <motion.div variants={boxVariant} initial="hidden" animate="visible">
         <h1 className=" mb-2 text-[24px] font-semibold leading-[29px] text-[#f5f5f5]">
           iPhone 12 Pro
         </h1>
         <h2 className=" text-[56px] font-semibold text-[#f5f5f5] md:text-[80px] md:leading-[88px]">
           Its a leap year.
         </h2>
-      </motion.div>
+      </motion.div> */}
+      <WavyText
+        text={"iPhone 12 Pro"}
+        replay={useTimeout(3)}
+        className="mb-2 text-[24px] font-semibold leading-[29px] text-[#f5f5f5]"
+      />
+      <WavyText
+        text={"Its a leap year."}
+        replay={useTimeout(3)}
+        className="text-center text-[45px] font-semibold text-[#f5f5f5] md:text-[80px] md:leading-[88px]"
+      />
 
       <div
         style={{
@@ -35,7 +49,7 @@ function Hero({ boxVariant, videoRef }: Props) {
           style={{
             transition: "transform 0.5s ease",
           }}
-          className="hero-video-wrap mx-auto md:-mt-32"
+          className="mx-auto hero-video-wrap md:-mt-32"
           src="/hero.mp4"
         />
       </div>
@@ -44,7 +58,7 @@ function Hero({ boxVariant, videoRef }: Props) {
         layers={[
           OpacityEffectAfter(
             <>
-              <h3 className="mx-auto w-4/6 text-2xl font-semibold text-white">
+              <h3 className="w-4/6 mx-auto text-2xl font-semibold text-white">
                 5G goes Pro. A14 Bionic rockets past every other smartphone
                 chip. The Pro camera system takes low-light photography to the
                 next level — with an even bigger jump on iPhone 12 Pro Max. And
@@ -68,3 +82,18 @@ function Hero({ boxVariant, videoRef }: Props) {
 }
 
 export default Hero;
+function useTimeout(seconds: number) {
+  const [isTimedOut, setIsTimedOut] = React.useState(false);
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsTimedOut(true);
+    }, seconds * 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [seconds]);
+
+  return isTimedOut;
+}
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -5,9 +5,9 @@ type Props = {};
 
 const NavBar = (props: Props) => {
   return (
-    <div className="m-auto max-w-screen-lg ">
+    <div className="max-w-screen-lg m-auto ">
       <div className=" flex h-[52px] items-center justify-between pl-[16px] pr-[16px]  md:pt-3 md:pl-[12px] md:pr-[12px]">
-        <Link href="/" className=" text-[21px] font-semibold">
+        <Link href="/" className=" text-[17px] font-semibold md:text-[21px]">
           {" "}
           iPhone 12 Pro
         </Link>
